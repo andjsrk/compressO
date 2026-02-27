37827c29047df63d8b38db8b338bc0340c72d533
niraj-khatiwada <nirazzz.nk@gmail.com>
2026-02-28 00:13:54 +0545

    feat: update dist directories

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,27 +1,27 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwindcss from "@tailwindcss/vite";
+
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  base: "/",
-  site: "https://compresso.codeforreal.com",
+  base: '/',
+  site: 'https://compresso.codeforreal.com',
   integrations: [mdx(), sitemap()],
-  output: "static",
-  outDir: "../dist",
+  output: 'static',
+  outDir: 'dist',
   vite: {
     plugins: [tailwindcss()],
     build: {
       rollupOptions: {
-        external: ["@resvg/resvg-js"],
+        external: ['@resvg/resvg-js'],
       },
     },
   },
   markdown: {
     shikiConfig: {
       theme: 'catppuccin-mocha',
-
     },
   },
-});
+})
